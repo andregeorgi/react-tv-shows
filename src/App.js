d18f7fee7c4923f738d4038ad41939e04c0b4fc2
Andrea Ciorban <andrea@192-168-0-106.rdsnet.ro>
2023-11-03 12:52:54 +0200

    styled favorites

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./views/homepage/Homepage";
+import SeasonsPage from "./views/seasons/SeasonPage";
+import EpisodesPage from "./views/episodes/EpisodesPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Homepage />} />
+        <Route path="/seasons/:showId" element={<SeasonsPage />} />
+        <Route path="/episodes/:seasonId" element={<EpisodesPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import { ShowsContext } from "../../context/ShowsContext";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import StarIcon from "@mui/icons-material/Star";
+
+function Favorites() {
+  const { favorites, removeFavorite } = useContext(ShowsContext);
+
+  const columns = [];
+  for (let i = 0; i < favorites.length; i += 4) {
+    columns.push(favorites.slice(i, i + 4));
+  }
+
+  return (
+    <div className="favorites-container">
+      <h2>My Favorites</h2>
+      <div className="favorites-columns">
+        {columns.map((column, index) => (
+          <div key={index} className="favorites-column">
+            {column.map((show) => (
+              <div key={show.id} className="favorite-item">
+                <ListItem disablePadding>
+                  <ListItemButton onClick={() => removeFavorite(show.id)}>
+                    <ListItemIcon>
+                      <StarIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={show.name} />
+                  </ListItemButton>
+                </ListItem>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Favorites;
